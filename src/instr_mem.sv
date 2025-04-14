307c1ac8a1953970a7f825a2155088f17fee6dfd
Marko Gjorgjievski <Gjorgjievski.m@protonmail.com>
2025-04-14 11:10:00 +0200

    Instruction memory: Top level PR clean-up.
    Reset removed

--- src/instr_mem.sv
+++ src/instr_mem.sv
@@ -1,8 +1,10 @@
 /*
     File name: instr_mem.sv
-    Description: This file contains the module for the instruction memory.
+    Description: Instruction memory is a storage unit used for
+                storing instructions to be read during program execution.
     Author: Marko Gjorgjievski
-    Date: 15.01.2025
+    Date created: 15.01.2025
+    Date modified: 13.04.2025
 */
 
 import pkg_config::*;
@@ -12,20 +14,11 @@ module instruction_memory #(
     parameter MEM_SIZE = 1024
 )  (
   	input wire [$clog2(MEM_SIZE)-1:0] addr_i,
-    input wire rst_n_i,
   	output logic [INST_WIDTH-1:0] inst_o
 );
 
     // synthesis-friendly ROM using a 2D array
     logic [INST_WIDTH-1:0] memory [0:MEM_SIZE-1];
-    
-    always_ff @(negedge rst_n_i) begin
-        if (!rst_n_i) begin
-            for (int i = 0; i < MEM_SIZE; i++) begin
-                memory[i] <= '0; // Set each memory location to 0
-            end
-        end
-    end
 
     // Instruction output register dependent on memory register.
     assign inst_o = memory[addr_i >> 2]; 
